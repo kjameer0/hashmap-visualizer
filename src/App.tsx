1ae5776af4d2a20983d1875a909de82076501528
Khalid Jameer <kjameer0@gmail.com>
2023-02-09 19:03:36 -0500

    added basic error router page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { Header } from 'components/Header';
 import { Button } from 'components/Button';
 import { ReactComponent as Logo } from 'assets/favicon.svg';
 
+
 function App() {
   return (
     <div className="App">
       <Header title="hola" />
       <Logo height={100} width={100} />
-      <Button onClick={() => alert('hola')}>hey</Button>
+      <Button onClick={() => alert('hola')}>hy</Button>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,26 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import 'tailwindcss/tailwind.css';
+import ErrorPage from 'utils/error-handling';
 
 import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
   root.render(
     <StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </StrictMode>
   );
 }
