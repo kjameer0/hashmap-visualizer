331f3512e892043ed8989764e663e38dfbaee977
Khalid Jameer <kjameer0@gmail.com>
2023-02-25 16:54:16 -0500

    feature: hashmap has, set, get, elete, keys, values, clear, size buttons

--- src/components/visualizers/hashmap.tsx
+++ src/components/visualizers/hashmap.tsx
@@ -1,32 +1,99 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { produceErrorMessage } from 'utils/arr-utils';
 import HashVis from 'components/HashVis';
 export default function HashMap() {
-  const [hashmap, setHashmap] = useState(
-    new Map([
-      ['key1', 'value1'],
-      ['key2', 'value2'],
-    ])
-  );
+  const [hashmap, setHashmap] = useState(new Map());
   const [hashKey, setHashKey] = useState('');
   const [hashVal, setHashVal] = useState('');
   const [errorText, setErrorText] = useState('');
   const [output, setOutPut] = useState('');
+  //error text is cleared if hashmap or output is updated
+  useEffect(() => {
+    setErrorText('');
+  }, [hashmap, output]);
+  useEffect(() => {
+    setOutPut('');
+  }, [errorText]);
+  function resetFields() {
+    // reset text fields
+    setHashKey('');
+    setHashVal('');
+  }
   function handleSet() {
     //checks if key exists, creates new map, adds new value, clears fields
     try {
       let val = hashVal;
       if (!hashKey) throw new Error('No Key Given');
+      //if there is no value in hashVal field default val to true
       if (val.length === 0) val = 'true';
-      const newMap = new Map([...hashmap]);
-      newMap.set(hashKey, val);
-      setHashmap(newMap);
-      setHashKey('');
-      setHashVal('');
+      //make a new map that copies old hashmap and adds new key-value
+      setHashmap(new Map([...hashmap, [hashKey, val]]));
+      resetFields();
     } catch (error) {
-      const err = produceErrorMessage(error);
-      setErrorText(err);
+      //set error text in component state to display error message
+      setErrorText(produceErrorMessage(error));
     }
+  }
+  //print map.get to output field
+  function handleGet() {
+    try {
+      if (!hashKey) throw new Error('No Key Given');
+      setOutPut(`Value: ${String(hashmap.get(hashKey))}`);
+      resetFields();
+    } catch (error) {
+      setErrorText(produceErrorMessage(error));
+    }
+  }
+  //print map.has to output field
+  function handleHasKey() {
+    try {
+      if (!hashKey) throw new Error('No Key Given');
+      setOutPut(String(hashmap.has(hashKey)));
+      resetFields();
+    } catch (error) {
+      setErrorText(produceErrorMessage(error));
+    }
+  }
+  function handleDelete() {
+    try {
+      if (!hashKey) throw new Error('No Key Given');
+      const newMap = new Map([...hashmap]);
+      newMap.delete(hashKey);
+      setHashmap(newMap);
+      resetFields();
+    } catch (error) {
+      setErrorText(produceErrorMessage(error));
+    }
+  }
+  //print all keys of map to output field
+  function handleKeys() {
+    try {
+      if (!hashmap.size) throw new Error('No hashmap to reference');
+      setOutPut(Array.from(hashmap.keys()).join(', '));
+    } catch (error) {
+      setErrorText(produceErrorMessage(error));
+    }
+  }
+  //print all values of map to output field
+  function handleValues() {
+    try {
+      if (!hashmap.size) throw new Error('No hashmap to reference');
+      setOutPut(Array.from(hashmap.values()).join(', '));
+      console.log(output);
+    } catch (error) {
+      setErrorText(produceErrorMessage(error));
+    }
+  }
+  //print number of elements in map to output field
+  function handleSize() {
+    try {
+      setOutPut(String(hashmap.size));
+    } catch (error) {
+      setErrorText(produceErrorMessage(error));
+    }
+  }
+  function handleClear() {
+    setHashmap(new Map());
   }
   return (
     <div className="container">
@@ -63,12 +130,27 @@ export default function HashMap() {
           <button className="main-button" onClick={handleSet}>
             Set
           </button>
-          <button className="main-button">Get</button>
-          <button className="main-button">Has Key</button>
-          <button className="main-button">Delete</button>
-          <button className="main-button">Keys</button>
-          <button className="main-button">Values</button>
-          <button className="main-button">Size</button>
+          <button className="main-button" onClick={handleGet}>
+            Get
+          </button>
+          <button className="main-button" onClick={handleHasKey}>
+            Has Key
+          </button>
+          <button className="main-button" onClick={handleDelete}>
+            Delete
+          </button>
+          <button className="main-button" onClick={handleKeys}>
+            Keys
+          </button>
+          <button className="main-button" onClick={handleValues}>
+            Values
+          </button>
+          <button className="main-button" onClick={handleSize}>
+            Size
+          </button>
+          <button className="main-button" onClick={handleClear}>
+            Clear
+          </button>
         </div>
         <div className="hash-output">
           <p>Error: {errorText}</p>
