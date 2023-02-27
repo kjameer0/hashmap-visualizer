b3a6351efbe8e2cb719db132830966b045a5d3e2
Khalid Jameer <kjameer0@gmail.com>
2023-02-27 15:32:41 -0500

    put nav bar on the side

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Header } from 'components/Header';
 import { Button } from 'components/Button';
 import NavBar from 'components/NavBar';
+import { useState } from 'react';
 import { ReactComponent as Logo } from 'assets/favicon.svg';
 import { Outlet } from 'react-router-dom';
 
 function App() {
+  const [open, setOpen] = useState(false);
   return (
     <div className="App">
-      <NavBar />
+      <button className="nav-button main-button" onClick={() => setOpen(!open)}>
+        Nav
+      </button>
+      <NavBar open={open} setOpen={setOpen} />
       <Outlet />
     </div>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,40 +1,56 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import CSS from 'csstype';
 //just make a cosmetic version
-export default function NavBar() {
+const barStyle: CSS.Properties = {
+  height: '100%',
+  width: '0',
+  position: 'fixed',
+  zIndex: 1,
+  top: '0',
+  left: '0',
+  backgroundColor: '#111',
+  overflowX: 'hidden',
+  paddingTop: '6px',
+  transition: '0.5s',
+};
+const liStyle: CSS.Properties = {
+  cursor: 'pointer',
+  fontSize: '1.3rem',
+  transition: '0.3s ease-in-out',
+  display: 'inline-block',
+  position: 'relative',
+  zIndex: 1,
+  padding: '2em 2em 1em 2em',
+  margin: '-2em -1em -1em -1em',
+};
+export default function NavBar({
+  open,
+  setOpen,
+}: {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}) {
   return (
-    <nav className={'nav-bar'}>
+    <nav className={'nav-bar'} style={{ ...barStyle, width: open ? '10rem' : '0rem' }}>
       <ul>
         <li>
-          <NavLink
-            className={'nh'}
-            to="/"
-            style={({ isActive }) =>
-              isActive ? { backgroundColor: 'white', height: '85px' } : undefined
-            }
-          >
+          <button className="nav-button" onClick={() => setOpen(!open)}>
+            Navigation
+          </button>
+        </li>
+        <li>
+          <NavLink className={'nh'} to="/" onClick={() => setOpen(!open)}>
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink
-            className={'nh'}
-            to="/arr"
-            style={({ isActive }) =>
-              isActive ? { backgroundColor: 'white', height: '85px' } : undefined
-            }
-          >
+          <NavLink className={'nh'} to="/arr" onClick={() => setOpen(!open)}>
             Array
           </NavLink>
         </li>
         <li>
-          <NavLink
-            className={'nh'}
-            to="/hashmap"
-            style={({ isActive }) =>
-              isActive ? { backgroundColor: 'white', height: '85px' } : undefined
-            }
-          >
+          <NavLink className={'nh'} to="/hashmap" onClick={() => setOpen(!open)}>
             HashMap
           </NavLink>
         </li>
